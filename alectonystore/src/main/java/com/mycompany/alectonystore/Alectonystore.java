9a69c164d4de786969cb56cd9ae4ca6b1890cd8b
Group2-System <erreldungca069@gmail.com>
2025-04-22 14:53:25 +0800

    testing

---
+++ alectonystore/src/main/java/com/mycompany/alectonystore/Alectonystore.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.alectonystore;
+
+/**
+ *
+ * @author CL3-PC10
+ */
+public class Alectonystore {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
